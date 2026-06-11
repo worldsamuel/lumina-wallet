4597d9e1e2d6b1e07c14e3733d107b9819a76909
Samuel <4tjctj6gdn@privaterelay.appleid.com>
2026-06-11 13:22:50 +0800

    Disable home page shell caching

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import { RouteView } from "@/components/lumina/route-view";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
+
 /**
  * Renders the Lumina v22 home view.
  */
